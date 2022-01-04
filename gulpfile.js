1a7f304a921ead67c19f2498259869ef036dcce8
Thomas Jakobi <thomas.jakobi@partout.info>
2022-01-04 01:23:00 +0100

    Switch from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+const gulp = require('gulp'),
+    autoprefixer = require('autoprefixer'),
+    composer = require('gulp-uglify/composer'),
+    concat = require('gulp-concat'),
+    cssnano = require('cssnano'),
+    footer = require('gulp-footer'),
+    format = require('date-format'),
+    header = require('gulp-header'),
+    postcss = require('gulp-postcss'),
+    rename = require('gulp-rename'),
+    replace = require('gulp-replace'),
+    sass = require('gulp-sass')(require('sass')),
+    uglifyjs = require('uglify-js'),
+    uglify = composer(uglifyjs, console),
+    pkg = require('./_build/config.json');
+
+const banner = '/*!\n' +
+    ' * <%= pkg.name %> - <%= pkg.description %>\n' +
+    ' * Version: <%= pkg.version %>\n' +
+    ' * Build date: ' + format("yyyy-MM-dd", new Date()) + '\n' +
+    ' */';
+
+gulp.task('scripts-web', function () {
+    return gulp.src([
+        'source/js/emo.js'
+    ])
+        .pipe(concat('emo.min.js'))
+        .pipe(uglify())
+        .pipe(header(banner + '\n', {pkg: pkg}))
+        .pipe(gulp.dest('assets/components/emo/js/'))
+});
+
+gulp.task('sass-web', function () {
+    return gulp.src([
+        'source/sass/emo.scss'
+    ])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
+        .pipe(gulp.dest('source/css/'))
+        .pipe(concat('emo.css'))
+        .pipe(postcss([
+            cssnano({
+                preset: ['default', {
+                    discardComments: {
+                        removeAll: true
+                    }
+                }]
+            })
+        ]))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(footer('\n' + banner, {pkg: pkg}))
+        .pipe(gulp.dest('assets/components/emo/css/'))
+});
+
+gulp.task('bump-copyright', function () {
+    return gulp.src([
+        'core/components/emo/model/emo/emo.class.php',
+        'core/components/emo/src/Emo.php',
+    ], {base: './'})
+        .pipe(replace(/Copyright 2011(-\d{4})? by/g, 'Copyright ' + (new Date().getFullYear() > 2011 ? '2011-' : '') + new Date().getFullYear() + ' by'))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump-version', function () {
+    return gulp.src([
+        'core/components/emo/src/Emo.php',
+    ], {base: './'})
+        .pipe(replace(/version = '\d+.\d+.\d+[-a-z0-9]*'/ig, 'version = \'' + pkg.version + '\''))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump-docs', function () {
+    return gulp.src([
+        'mkdocs.yml',
+    ], {base: './'})
+        .pipe(replace(/&copy; 2011(-\d{4})?/g, '&copy; ' + (new Date().getFullYear() > 2011 ? '2011-' : '') + new Date().getFullYear()))
+        .pipe(gulp.dest('.'));
+});
+gulp.task('bump', gulp.series('bump-copyright', 'bump-version', 'bump-docs'));
+
+
+gulp.task('watch', function () {
+    // Watch .js files
+    gulp.watch(['./source/js/**/*.js'], gulp.series('scripts-web'));
+    // Watch .scss files
+    gulp.watch(['./source/scss/**/*.scss'], gulp.series('sass-web'));
+});
+
+// Default Task
+gulp.task('default', gulp.series('bump', 'scripts-web', 'sass-web'));
